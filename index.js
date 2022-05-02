701a1e60580152ca7caba8a38a67100db8d83065
Gonuguntla Udaya Kiran <udaya.kirangonuguntla.20cse@bmu.edu.in>
2022-05-02 10:52:17 +0530

    Installing mongoose and connecting node to mongodb

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express(); 
 const hbs = require("hbs");
 const path = require("path");
+const mongoose = require('mongoose');
+try{
+    mongoose.connect('mongodb://localhost/wp_project')
+    .then(()=>{
+        console.log("Database Connected Successfully");
+    })
+    .catch(()=>{
+        console.log("Failed");
+    })
+}
+catch(ex){
+    console.log(ex);
+}
+
 app.set('view engine','hbs');
 const css_path = path.join(__dirname,'/public');
 app.use(express.static(css_path));
